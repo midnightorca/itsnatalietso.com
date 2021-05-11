2c846ffb61e5346abbbcca030694dc61ed992e81
Natalie Tso <natalietso16@gmail.com>
2021-05-10 18:52:59 -0700

    name space js

--- script.js
+++ script.js
@@ -1,41 +1,47 @@
-// for scrolling nav bar to change a when active
-window.addEventListener('DOMContentLoaded', () => {
-
-    const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-            const id = entry.target.getAttribute("id");
-            if (entry.intersectionRatio > 0) {
-                document.querySelector(`a[href="#${id}"].navLink`).classList.add("current");
-            } else {
-                document.querySelector(`a[href="#${id}"].navLink`).classList.remove("current")
-            }
-        })
-    })
-
-    document.querySelectorAll('section[id]').forEach((section) => {
-        observer.observe(section);
-    })
-})
+const portfolio = {};
 
 // functions for scrolling back to top
-const toTopButton = document.getElementById("toTop");
+portfolio.toTopButton = document.getElementById("toTop");
 
-window.onscroll = function() {scrollFunc()}
-
-const scrollFunc = () => {
+portfolio.scrollFunc = () => {
     if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
-        toTopButton.classList.add("show");
+        portfolio.toTopButton.classList.add("show");
     } else {
-        toTopButton.classList.remove("show");
+        portfolio.toTopButton.classList.remove("show");
     }
 }
 
-const toTopFunc = () => {
+portfolio.toTopFunc = () => {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
 
 toTopButton.onclick = function(e) {
-    toTopFunc();
+    portfolio.toTopFunc();
 }
 
+portfolio.init = () => {
+    window.onscroll = function() {
+        portfolio.scrollFunc()
+    };
+
+    // for scrolling nav bar to change a when active
+    window.addEventListener('DOMContentLoaded', () => {
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                const id = entry.target.getAttribute("id");
+                if (entry.intersectionRatio > 0) {
+                    document.querySelector(`a[href="#${id}"].navLink`).classList.add("current");
+                } else {
+                    document.querySelector(`a[href="#${id}"].navLink`).classList.remove("current")
+                }
+            })
+        })
+
+        document.querySelectorAll('section[id]').forEach((section) => {
+            observer.observe(section);
+        })
+    })
+}
+
+portfolio.init();
